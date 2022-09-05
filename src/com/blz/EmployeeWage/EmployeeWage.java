d279a24d597e588befac1ebf48011fbdb6b0c60d
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-05 22:40:36 +0530

    Added: Employee Present/Absent logic

--- src/com/blz/EmployeeWage/EmployeeWage.java
+++ src/com/blz/EmployeeWage/EmployeeWage.java
@@ -1,9 +1,18 @@
 package com.blz.EmployeeWage;
-
+import java.util.Random;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!");
+		
+		Random random = new Random();
+		int randomNum = random.nextInt(2);
+		
+		if(randomNum == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+		
 
 	}
 
