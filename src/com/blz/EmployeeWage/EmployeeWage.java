460549512065a212e2c0ccd4384d386fa98279b6
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-05 22:55:20 +0530

    Added: Employee full time salary logic

--- src/com/blz/EmployeeWage/EmployeeWage.java
+++ src/com/blz/EmployeeWage/EmployeeWage.java
@@ -5,14 +5,21 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!");
 		
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int salary = 0;
+		
+		
 		Random random = new Random();
 		int randomNum = random.nextInt(2);
 		
-		if(randomNum == 1)
+		if(randomNum == 1) {
 			System.out.println("Employee is Present");
+		salary= wagePerHour * fullDayHour;
+		}
 		else
 			System.out.println("Employee is Absent");
-		
+		    System.out.println("Employee salary is "+salary);
 
 	}
 
