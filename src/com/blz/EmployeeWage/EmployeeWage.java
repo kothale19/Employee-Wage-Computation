c19de028aceccf9bb4bc5318c9ad78df5997876a
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-06 00:02:58 +0530

    Added: Employee Monthly wage logic

--- src/com/blz/EmployeeWage/EmployeeWage.java
+++ src/com/blz/EmployeeWage/EmployeeWage.java
@@ -9,24 +9,30 @@ public class EmployeeWage {
 		int fullDayHour = 8;
 		int halfDayHour = 4;
 		int salary = 0;
-		
+		int workingDayPerMonth = 20;
+		int monthlySalary = 0;
 		
 		Random random = new Random();
 		int randomNum = random.nextInt(3);
 		
+		for(int i=1; i<=workingDayPerMonth; i++)
+		{		
 		switch(randomNum) 
 		{
-		case 0: System.out.println("Employee is Absent");
+		case 0: 
+			System.out.println("Employee is Absent");
 		    break;
-		case 1: System.out.println("Employee is Present");
-		salary= wagePerHour * fullDayHour;
+		case 1: 
+			System.out.println("Employee is Present");
+		    salary= wagePerHour * fullDayHour;
 		    break;
 		case 2: salary= wagePerHour * halfDayHour;
 		     break;
 		}
+	      monthlySalary = monthlySalary + salary;
+		}
 	
-	
-		   System.out.println("Employee salary is: "+salary);
+		   System.out.println("Employee monthlySalary is: "+monthlySalary);
 
 	}
 
